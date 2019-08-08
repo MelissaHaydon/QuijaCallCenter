0091230bbc744245a129f1e49a8e1928109e0d72
Melissa <mvhaydon@gmail.com>
2019-08-08 14:48:27 +1200

    New Layout and Art

---
+++ OuijaCallCenter/Assets/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    enum TutorialState {Intro, AnswerCalls, BuyCharacter}
+    TutorialState tutState;
+
+    string introText01;
+    string introText02;
+
+    void Start()
+    {
+        tutState = TutorialState.Intro;
+
+    }
+
+
+    void Update()
+    {
+        if(tutState == TutorialState.Intro)
+        {
+
+        }
+    }
+}
