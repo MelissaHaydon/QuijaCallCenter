aedcebdd94f12cafe2e7738f2944b0569b08606a
Melissa <mvhaydon@gmail.com>
2019-08-08 15:23:20 +1200

    Tutorial Started

---
+++ OuijaCallCenter/Assets/Scripts/BuyNewEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyNewEmployee : MonoBehaviour
+{
+
+    public Sprite ghostEmployee;
+    public ParticleSystem cloud;
+
+    void Start()
+    {
+        cloud.gameObject.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void PurchaseEmployee()
+    {
+        
+        cloud.gameObject.SetActive(true);
+        cloud.Play();
+    }
+}
