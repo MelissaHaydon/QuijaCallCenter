0e78603814c1b59a702db12f0b184544d08b99cf
Melissa <mvhaydon@gmail.com>
2019-08-22 14:59:10 +1200

    More Characters Added and Camera Pan

---
+++ OuijaCallCenter/Assets/Scripts/Tutorial/CameraPan.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+
+    public float pannSpeed = 10f;
+    public float panBorder = 10f;
+
+    public Vector2 panLimit;
+    public float minZ = -12f;
+    public float maxZ = -2f;
+
+    public float scrollSpeed = 2;
+
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorder)
+        {
+            pos.y += pannSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorder)
+        {
+            pos.y -= pannSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorder)
+        {
+            pos.x += pannSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorder)
+        {
+            pos.x -= pannSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.z += scroll * scrollSpeed * 100f * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
+
+        transform.position = pos;
+    }
+}
